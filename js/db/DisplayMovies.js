09d06f3b3083d604a6021bcba931568928eb60ac
FrancoMarchetta <frncmarchetta@gmail.com>
2026-06-02 16:55:42 -0300

    feat: verifico si la pelicula esta marcada como visible en la db antes de mostrarla

--- js/db/DisplayMovies.js
+++ js/db/DisplayMovies.js
@@ -1,6 +1,6 @@
 import { fetchData } from "/js/db/DataBaseMethods.js";
 
-const movies = await fetchData();
+let movies = await fetchData();
 console.log(movies);
 
 // contenedor en Home page
@@ -10,39 +10,31 @@ let $homePageMovieContainer = document.getElementById(
 //contenedor en pagina de cartelera
 let $moviesContainer = document.getElementById("movies-grid");
 
-// for (let movie of movies.slice(0, 3)) {
-//   $homePageMovieContainer.insertAdjacentHTML(
-//     "beforeend",
-//     `
-//         <div class="movie">
-//             <a href="/movie-detail.html?id=${movie.id}">
-//                 <img class="movie-image" src="${"https://i.blogs.es/c7ed10/screenshot_90/1366_2000.webp"}" alt="${movie.titulo}">
-//                 <h3>${movie.titulo}</h3>
-//                 <p>${movie.genero} | ${movie.duracion} min</p>
-//             </a>
-//         </div>
-//     `,
-//   );
-// }
-
-function displayMovies($container, limit) {
+function displayMovies($container, limit = null) {
   if (!$container) {
     return;
   }
-  for (let movie of movies.slice(0, limit)) {
-    $container.insertAdjacentHTML(
-      "beforeend",
-      `
+  let displayedCount = 0;
+  movies.sort();
+  for (let movie of movies) {
+    if (limit !== null && displayedCount >= limit) break;
+    if (movie.visible) {
+      $container.insertAdjacentHTML(
+        "beforeend",
+        `
         <div class="movie">
-            <a href="/movie-detail.html?id=${movie.id}">
-                <img class="movie-image" src="${"https://i.blogs.es/c7ed10/screenshot_90/1366_2000.webp"}" alt="${movie.titulo}">
-                <h3>${movie.titulo}</h3>
-                <p>${movie.genero} | ${movie.duracion} min</p>
-            </a>
+        <a href="/movie-detail.html?id=${movie.id}">
+        <img class="movie-image" src="${"https://i.blogs.es/c7ed10/screenshot_90/1366_2000.webp"}" alt="${movie.titulo}">
+        <h3>${movie.titulo}</h3>
+        <p>${movie.genero} | ${movie.duracion} min</p>
+        </a>
         </div>
-    `,
-    );
+        `,
+      );
+      displayedCount++;
+    }
   }
 }
+
 displayMovies($homePageMovieContainer, 3);
-displayMovies($moviesContainer, 10);
+displayMovies($moviesContainer);
